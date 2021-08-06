3ced16133d79a7b0abba8c1789d712a98cb9e526
Alfonzo28 <alfonzolouw9@gmail.com>
2021-08-06 10:29:34 +0200

    Email input update and Logo animation

--- ardesignstudio/src/App.js
+++ ardesignstudio/src/App.js
@@ -5,18 +5,18 @@ import Contact from './Contact.svg';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
-        <p className="fontStyle1">
-          Coming Soon
-        </p>
- <form className="Contactform">
+       <div className="fontStyle1">
+         Coming Soon
+       </div>
 
+
+ <form className="Contactform">
+    
         <label>
         Notify me when you’re up:</label>
         <div className="EmailContainer">
-    <input className="ContainerStyle" type="Email" name="Email" placeholder="Your Email Address"/>
+    <input className="EmailInput" type="Email" name="Email" placeholder="Your Email Address"/>
     <input className="ButtonStyle"  type="submit" value="Notify Me"/> 
     </div>
 </form>
@@ -25,8 +25,6 @@ function App() {
 <img src={Contact} className="Contact" alt="contact"/>
 
 
-
-      </header>
     </div>
   );
 }
